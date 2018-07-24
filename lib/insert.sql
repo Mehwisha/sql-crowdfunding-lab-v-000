454377f22b3869dde5c3e51c7fc275bad0c7341b
Mehwish Aziz <mehwisha@msn.com>
2018-07-24 03:38:32 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,24 @@
-INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (001,'PMO','Music', '$1000', 05/29/2018, 06/05/2018);
-INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (002,'programming','books', '$100', 05/29/2017, 06/05/2017);
-INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (003,'DEV','Drama', '$9000', 07/29/2018, 08/05/2018);
-INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (004,'PROD','Ball', '$500', 08/29/2018, 09/05/2018);
-INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (005,'FOOD','Caps', '$300', 09/29/2018, 10/05/2018);
-INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (006,'CAR','Caps', '$2500', 10/29/2018, 11/05/2018);
-INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (007,'CDR','Drama', '$100', 11/29/2018, 12/05/2018);
-INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (008,'CDY','Development', '$150', 12/29/2018, 01/05/2019);
-INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (009,'TREE','Sports', '$180', 01/29/2019, 02/05/2019);
-INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (0010,'LESS','Ball', '$10', 02/25/2019, 06/05/2019);
+-- INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (001,'PMO','Music', '$1000', 05/29/2018, 06/05/2018);
+-- INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (002,'programming','books', '$100', 05/29/2017, 06/05/2017);
+-- INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (003,'DEV','Drama', '$9000', 07/29/2018, 08/05/2018);
+-- INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (004,'PROD','Ball', '$500', 08/29/2018, 09/05/2018);
+-- INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (005,'FOOD','Caps', '$300', 09/29/2018, 10/05/2018);
+-- INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (006,'CAR','Caps', '$2500', 10/29/2018, 11/05/2018);
+-- INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (007,'CDR','Drama', '$100', 11/29/2018, 12/05/2018);
+-- INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (008,'CDY','Development', '$150', 12/29/2018, 01/05/2019);
+-- INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (009,'TREE','Sports', '$180', 01/29/2019, 02/05/2019);
+-- INSERT into projects(id, title, category, Funding_goal, Start_date, end_date) values (0010,'LESS','Ball', '$10', 02/25/2019, 06/05/2019);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Ocean Cleanup", "Environment", 400, "6/1/17", "6/30/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cat & Dog Food Drive", "Animals", 500, "6/1/17", "6/30/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Toys for Children", "Children", 125, "6/1/17", "6/30/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Meals on Wheels", "Homeless", 75, "6/1/17", "6/30/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Alzheimers Run", "Seniors", 100, "6/1/17", "6/30/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Counseling Sessions", "Domestic Violence", 65, "7/1/17", "7/31/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Wig Making", "Cancer Survivors", 1000, "7/1/17", "7/31/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog Harnesses", "Blind & Deaf", 500, "7/1/17", "7/31/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Musical Instruments", "Developmental Disabilities", 315, "7/1/17", "7/31/17");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("PTSD Counseling", "Military", 80, "7/1/17"
+
 
 INSERT into USERS values (1, "Jennifer", 25);
 INSERT into USERS values (2, "Conner", 25);
