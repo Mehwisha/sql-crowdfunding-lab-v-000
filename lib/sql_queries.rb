c54ddc23306f3ad69c35cc86cb48307e055981c8
Mehwish Aziz <mehwisha@msn.com>
2018-07-24 03:31:57 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
- "Select projects.category, SUM(pledges.amount) total_pledges from projects
+ "Select projects.category, SUM(pledges.amount) as total_pledges from projects
  INNER JOIN pledges
  ON projects.id = pledges.project_id
  where category = 'books;'"
